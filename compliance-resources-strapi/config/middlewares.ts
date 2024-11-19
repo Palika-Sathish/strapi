aa35d86f45b39b42ece2c1c873986612c774069d
Palika-Sathish <113234133+Palika-Sathish@users.noreply.github.com>
2024-11-19 11:09:58 -0500

    Update middlewares.ts

--- compliance-resources-strapi/config/middlewares.ts
+++ compliance-resources-strapi/config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4200'], // Add the URL of your Angular app
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
