4e66fbde4bbf28375b5dac26d44f45ef4a614cde
Palika-Sathish <113234133+Palika-Sathish@users.noreply.github.com>
2024-11-19 12:17:19 -0500

    Update middlewares.ts

--- compliance-resources-strapi/config/middlewares.ts
+++ compliance-resources-strapi/config/middlewares.ts
@@ -1,39 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
-        'http://localhost:4200', // Angular development server
-        'http://fnhatest.compliance365.org:4200', // Test environment
-        'https://fnhastage.compliance365.ca', // Staging environment
-        'https://fnhatest.compliance365.ca', // Production environment
-      ],
+        'http://fnhatest.compliance365.org:4200', 
+        'https://fnhastage.compliance365.ca',
+        'https://fnhatest.compliance365.ca'
+      ], // Add all your URLs here
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: [
-        'Content-Type',
-        'Authorization',
-        'Origin',
-        'Accept',
-        'X-Requested-With', // Custom headers
-      ],
-      keepHeaderOnError: true,
-      credentials: true, // If using session-based authentication, this allows cookies/headers
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
     },
   },
   'strapi::poweredBy',
